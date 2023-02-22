e65a479c80ba7c3b44389c30a93292c46a3ea05f
cyun9601 <cyun9601@gmail.com>
2023-02-22 04:24:12 +0000

    Change class architectures

--- pcb_parser/draw.py
+++ pcb_parser/draw.py
@@ -5,81 +5,15 @@ from dtypes import Shape, PCB, Arc, Line
 import json
 from matplotlib.patches import Arc as m_Arc
 from matplotlib.lines import Line2D
+import os 
 
-def plot_line(lines: list[Line], ax, shift=None):
-    for line in lines:
-        if shift is not None:
-            l = Line2D(*line.move(shift))
-        else:
-            l = Line2D(*line.get_line())
-        ax.add_line(l)
-
-def plot_arc(arcs:list[Arc], ax, shift=None):
-    for arc in arcs: 
-        if shift is not None: 
-            center = arc.move(shift)
-        else : 
-            center = arc.get_center()
-        width = arc.radius * 2
-        height = arc.radius * 2
-        angle = 0
-        if arc.direction == 'CW': 
-            theta1 = arc.eAngle
-            theta2 = arc.sAngle
-        elif arc.direction == 'CCW':
-            theta1 = arc.sAngle
-            theta2 = arc.eAngle
-            
-        ax.add_patch(m_Arc(center, width, height, angle, theta1, theta2))
-
-def draw_shape(comp, ax, shift=None):
-    ## line plot 
-    # multi_lines = MultiLineString([line.get_line for line in comp.lines])
-    # plot_line(multi_lines, ax=ax)
-    plot_line(comp.lines, ax, shift)
-
-    ## arc plot
-    plot_arc(comp.arcs, ax, shift)
-
-def draw_component(component_dict:dict, ax):
-    for component in component_dict.values():
-        draw_shape(component.component_top_shape, ax, (component.x, component.y))
-
-def show(board:Shape, hole_area:Shape, component_dict:dict, net_list:dict):
-    fig = plt.figure(1, figsize=(10, 10), dpi=90)
-
-    ax = fig.add_subplot(111)
-
-    ## draw board
-    draw_shape(board, ax)
-    
-    ## draw hole area 
-    draw_shape(hole_area, ax)
-    
-    ## draw component_dict 
-    draw_component(component_dict, ax)
-    # draw_shape(list(component_dict.values())[0].component_top_shape, ax, (list(component_dict.values())[0].x, list(component_dict.values())[0].y))
-    # draw_shape(component_dict, ax)
-    
-    # ax.add_patch(arc)
-    ax.set_title('a) valid')
-    # 축의 범위 설정
-    ax.set_xlim([0, 60])
-    ax.set_ylim([0, 150])
-    ax.set_aspect('equal') #, 'box')
-    
-    plt.savefig(dpi=300, fname='shapely_polygon.png')
-
-     
-    
 if __name__=="__main__": 
 
+    os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
     with open("./data/sample_data.json", 'r') as f:
-            data = json.load(f)
+        data = json.load(f)
 
     pcb = PCB(list(data.values())[0])
-
-    show(pcb.board, pcb.hole_area, pcb.component_dict, pcb.net_list)
-    
-
+    pcb.draw()
     
--- pcb_parser/dtypes.py
+++ pcb_parser/dtypes.py
@@ -1,8 +1,12 @@
 import json 
 from pathlib import Path
 from dataclasses import dataclass
+from abc import *
+import matplotlib.pyplot as plt
+from matplotlib.patches import Arc as mpl_Arc
+from matplotlib.lines import Line2D
 
-class D_type:
+class Geometry(metaclass = ABCMeta):
     def __init__(self, **kwargs) -> None:
         self.type = kwargs['type']
         self.startX = float(kwargs['StartX'])
@@ -10,14 +14,23 @@ class D_type:
         self.endX = float(kwargs['EndX'])
         self.endY = float(kwargs['EndY'])
 
-class Line(D_type):
+    @abstractmethod
+    def translation(self):
+        ...
+
+class Draw_type(metaclass = ABCMeta):
+    @abstractmethod
+    def draw(self):
+        ...
+
+class Line(Geometry, Draw_type):
     def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
+        Geometry.__init__(self, **kwargs)
 
     def get_line(self):
         return (self.startX, self.endX), (self.startY, self.endY)
 
-    def move(self, shift, inplace = False):
+    def translation(self, shift, inplace = False):
         assert len(shift) == 2, "shift must be 2-dimension"
         if inplace == False:
             return (self.startX + shift[0], self.endX + shift[0]), (self.startY + shift[1], self.endY + shift[1])
@@ -27,10 +40,17 @@ class Line(D_type):
             self.startY += shift[1]
             self.endY += shift[1]
             return self.get_line()
+    
+    def draw(self, ax, shift = None):
+        if shift is not None:
+            l = Line2D(*self.translation(shift))
+        else:
+            l = Line2D(*self.get_line())
+        ax.add_line(l)
             
-class Arc(D_type):
+class Arc(Geometry, Draw_type):
     def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
+        Geometry.__init__(self, **kwargs)
         self.radius = float(kwargs['Radius'])
         self.sAngle = float(kwargs['SAngle'])
         self.eAngle = float(kwargs['EAngle'])
@@ -41,7 +61,7 @@ class Arc(D_type):
     def get_center(self):
         return (self.centerX, self.centerY)
     
-    def move(self, shift, inplace = False):
+    def translation(self, shift, inplace = False):
         if inplace == False: 
             return (self.centerX + shift[0], self.centerY + shift[1])
         else :
@@ -49,8 +69,25 @@ class Arc(D_type):
             self.centerY += shift[1]
             return self.get_center() 
             
-    
-class Shape:
+    def draw(self, ax, shift = None):
+        if shift is not None: 
+            center = self.translation(shift)
+        else : 
+            center = self.get_center()
+        width = self.radius * 2
+        height = self.radius * 2
+        angle = 0
+        
+        if self.direction == 'CW' or self.direction == None:
+            theta1 = self.eAngle
+            theta2 = self.sAngle
+        elif self.direction == 'CCW':
+            theta1 = self.sAngle
+            theta2 = self.eAngle
+        
+        ax.add_patch(mpl_Arc(center, width, height, angle, theta1, theta2))
+            
+class Shape(Draw_type):
     def __init__(self, shape_info:dict) -> None:
         ## raw dict
         self.shape_info = shape_info
@@ -70,6 +107,12 @@ class Shape:
         
         ## processed data
         self.lines, self.arcs = self._parsing_component(self.shape_info)        
+        
+    def draw(self, ax, shift=None):
+        for line in self.lines:
+            line.draw(ax, shift=shift)
+        for arc in self.arcs:
+            arc.draw(ax, shift=shift)
         
     @staticmethod
     def _parsing_component(shape_info):
@@ -91,7 +134,7 @@ class Net:
         self.name = net_info['Name']
         self.pin_no = net_info['PinNo']
 
-class Component:
+class Component(Draw_type):
     def __init__(self, component_info:dict) -> None:
         self.part_number = int(component_info['PartNo'])
         self.name = component_info['Name']
@@ -114,7 +157,22 @@ class Component:
         self.fixed = component_info['Fixed']
         self.group = component_info['Group']
 
-class PCB:
+    def draw(self, ax, shift=None): 
+        if self.placed_layer == 'TOP':
+            self.component_top_shape.draw(ax, shift=(self.x, self.y))
+        elif self.placed_layer == 'BOTTOM':
+            self.component_bottom_shape.draw(ax, shift=(self.x, self.y))
+        self.hole_area.draw(ax, shift=shift)
+
+class Components(Draw_type):
+    def __init__(self, components_info:list[dict]) -> None:
+        self.components = [Component(comp_info) for comp_info in components_info]
+    
+    def draw(self, ax, shift=None):
+        for component in self.components:
+            component.draw(ax, shift=shift)
+
+class PCB(Draw_type):
     def __init__(self, pcb_info:dict) -> None:
         
         self.pcb_info = pcb_info
@@ -123,9 +181,25 @@ class PCB:
         self.board = Shape(pcb_info['BOARD_FIGURE'])
         self.hole_area = Shape(pcb_info['HoleArea'])
         self.prohibit_area = Shape(pcb_info['ProhibitArea'])
-        self.component_dict = dict(zip(pcb_info['ComponentDict'].keys(), [Component(comp) for comp in list(pcb_info['ComponentDict'].values())]))
+        self.components = Components(pcb_info['ComponentDict'].values()) 
         self.net_list = dict(zip(pcb_info['NetDict'].keys(), [Net(net_info) for net_info in list(pcb_info['NetDict'].values())]))
+    
+    def draw(self, shift=None, figsize=(10, 10), dpi=300) -> dict:
+        fig = plt.figure(1, figsize=figsize, dpi=dpi)
+
+        ax = fig.add_subplot(111)
         
+        self.board.draw(ax, shift=shift)
+        self.hole_area.draw(ax, shift=shift)
+        self.components.draw(ax, shift=shift)
+        
+        # ax.set_title('a) valid')
+        ax.set_xlim([0, 60])
+        ax.set_ylim([0, 150])
+        ax.set_aspect('equal') #, 'box')
+        
+        plt.savefig(dpi=dpi, fname='shapely_polygon.png')
+    
 if __name__=="__main__": 
     
     with open("./data/sample_data.json", 'r') as f:
